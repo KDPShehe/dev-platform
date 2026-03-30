2803b44b24309434345d2b6c885f1d5fede81d93
KDPShehe <92593683+KDPShehe@users.noreply.github.com>
2026-03-30 15:25:30 +0300

    AsyncPipe and add loading spinner state

--- src/app/features/resources/list/list.ts
+++ src/app/features/resources/list/list.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core'; 
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 import { CardComponent } from '../../../shared/components/card/card'; 
 import { WebResource, ResourceType } from '../../../shared/models/web-resource';
 import { ResourceService } from '../../../shared/services/resource'; 
@@ -7,40 +9,38 @@ import { ResourceService } from '../../../shared/services/resource';
 @Component({
   selector: 'dev-list',
   standalone: true,
-  imports: [CardComponent, FormsModule],
+  imports: [CardComponent, FormsModule, CommonModule],
   templateUrl: './list.html',
   styleUrls: ['./list.css']
 })
-
 export class ListComponent implements OnInit {
   private resourceService = inject(ResourceService);
 
-  filteredResources: WebResource[] = [];
+  resources$!: Observable<WebResource[] | null>;
   
   searchQuery: string = '';
   selectedCategory: ResourceType | 'All' = 'All';
   resourceTypes = Object.values(ResourceType);
 
   ngOnInit(): void {
-    this.filteredResources = this.resourceService.getAll();
+    this.resources$ = this.resourceService.items$;
   }
 
   filterItems(): void {
-    this.filteredResources = this.resourceService.filterResources(this.searchQuery, this.selectedCategory);
+    this.resourceService.updateFilters({
+      query: this.searchQuery,
+      category: this.selectedCategory as ResourceType | 'All'
+    });
   }
 
   resetFilters(inputElement: HTMLInputElement): void {
     this.searchQuery = '';
     this.selectedCategory = 'All';
-    this.filteredResources = this.resourceService.getAll(); 
+    this.filterItems();
     inputElement.focus();
   }
 
   handleCardAction(id: string): void {
     this.resourceService.deleteItem(id);
-
-    this.filterItems();
-    
-    console.log(`[DevSpace] Ресурс з ID: ${id} успішно видалено.`);
   }
 }
