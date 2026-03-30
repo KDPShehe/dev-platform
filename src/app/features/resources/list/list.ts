e827b6ae1b034ec9c12eb15569d40500bcfe139a
KDPShehe <92593683+KDPShehe@users.noreply.github.com>
2026-03-30 14:31:49 +0300

    ResourceService and update logic using ngOnInit

--- src/app/features/resources/list/list.ts
+++ src/app/features/resources/list/list.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core'; 
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from '../../../shared/components/card/card'; 
 import { WebResource, ResourceType } from '../../../shared/models/web-resource';
-import { WEB_RESOURCES } from '../../../shared/mock-data';
+import { ResourceService } from '../../../shared/services/resource'; 
 
 @Component({
   selector: 'dev-list',
@@ -11,37 +11,36 @@ import { WEB_RESOURCES } from '../../../shared/mock-data';
   templateUrl: './list.html',
   styleUrls: ['./list.css']
 })
-export class ListComponent {
-  allResources: WebResource[] = WEB_RESOURCES;
-  filteredResources: WebResource[] = [...this.allResources];
 
+export class ListComponent implements OnInit {
+  private resourceService = inject(ResourceService);
+
+  filteredResources: WebResource[] = [];
+  
   searchQuery: string = '';
   selectedCategory: ResourceType | 'All' = 'All';
-
   resourceTypes = Object.values(ResourceType);
 
-  filterItems() {
-    const query = this.searchQuery.toLowerCase().trim();
-
-    this.filteredResources = this.allResources.filter(item => {
-      const matchesSearch = item.title.toLowerCase().includes(query);
-
-      const matchesCategory = this.selectedCategory === 'All' || item.type === this.selectedCategory;
-
-      return matchesSearch && matchesCategory;
-    });
+  ngOnInit(): void {
+    this.filteredResources = this.resourceService.getAll();
   }
 
-  resetFilters(inputElement: HTMLInputElement) {
+  filterItems(): void {
+    this.filteredResources = this.resourceService.filterResources(this.searchQuery, this.selectedCategory);
+  }
+
+  resetFilters(inputElement: HTMLInputElement): void {
     this.searchQuery = '';
     this.selectedCategory = 'All';
-    this.filteredResources = [...this.allResources];
-
+    this.filteredResources = this.resourceService.getAll(); 
     inputElement.focus();
   }
 
-  handleCardAction(id: string) {
-    console.log(`[DevSpace Action] Користувач натиснув кнопку на ресурсі з ID: ${id}`);
-    alert(`Ресурс з ID: ${id} успішно оброблено!`);
+  handleCardAction(id: string): void {
+    this.resourceService.deleteItem(id);
+
+    this.filterItems();
+    
+    console.log(`[DevSpace] Ресурс з ID: ${id} успішно видалено.`);
   }
 }
