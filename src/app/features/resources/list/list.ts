4c13879fc4fecdf43e7226f991be64d92b68f605
KDPShehe <92593683+KDPShehe@users.noreply.github.com>
2026-03-30 13:46:28 +0300

    FormsModule and two-way binding for search input

--- src/app/features/resources/list/list.ts
+++ src/app/features/resources/list/list.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CardComponent } from '../../../shared/components/card/card'; 
-import { WebResource } from '../../../shared/models/web-resource';
+import { WebResource, ResourceType } from '../../../shared/models/web-resource';
 import { WEB_RESOURCES } from '../../../shared/mock-data';
 
 @Component({
   selector: 'dev-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, FormsModule],
   templateUrl: './list.html',
   styleUrls: ['./list.css']
 })
 export class ListComponent {
-  resourcesList: WebResource[] = WEB_RESOURCES;
+  allResources: WebResource[] = WEB_RESOURCES;
+  filteredResources: WebResource[] = [...this.allResources];
+
+  searchQuery: string = '';
+  selectedCategory: ResourceType | 'All' = 'All';
+
+  resourceTypes = Object.values(ResourceType);
+
+  filterItems() {
+    const query = this.searchQuery.toLowerCase().trim();
+
+    this.filteredResources = this.allResources.filter(item => {
+      const matchesSearch = item.title.toLowerCase().includes(query);
+
+      const matchesCategory = this.selectedCategory === 'All' || item.type === this.selectedCategory;
+
+      return matchesSearch && matchesCategory;
+    });
+  }
+
+  resetFilters(inputElement: HTMLInputElement) {
+    this.searchQuery = '';
+    this.selectedCategory = 'All';
+    this.filteredResources = [...this.allResources];
+
+    inputElement.focus();
+  }
 
   handleCardAction(id: string) {
     console.log(`[DevSpace Action] Користувач натиснув кнопку на ресурсі з ID: ${id}`);
-    alert(`Ресурс з ID: ${id} успішно оброблено! (Перевір консоль)`);
+    alert(`Ресурс з ID: ${id} успішно оброблено!`);
   }
 }
