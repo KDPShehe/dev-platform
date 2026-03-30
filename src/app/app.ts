3b439a7ac459ff8417017b96d34d5d4e0400704c
KDPShehe <92593683+KDPShehe@users.noreply.github.com>
2026-03-30 18:22:08 +0300

    other changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header'; 
 import { FooterComponent } from './core/footer/footer';
-import { ListComponent } from './features/resources/list/list';
 
 @Component({
   selector: 'dev-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ListComponent], 
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], 
   templateUrl: './app.html', 
   styleUrls: ['./app.css']
 })
